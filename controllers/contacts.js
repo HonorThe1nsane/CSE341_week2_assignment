9a6bf084408213494f9daed90eda439fa45002a5
HonorThe1nsane <troymd2009@hotmail.com>
2023-02-04 20:28:58 -0700

    updated POST, PUT, and DELETE methods

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -32,4 +32,43 @@ const getSingleData = async (req, res) => {
 
 };
 
-module.exports = { getSingleData, getData};
+const createNewContact = async (req, res) => {
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+  const response = await mongodb.getDb().db().collection('people').insertOne(contact);
+  if (response.acknowledged) {
+    res.status(201).json(response);
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while creating the contact.');
+  }
+};
+
+const updatePerson = async (req, res) => {
+  if (!req.body) {
+    return res.status(400).send({
+      message: 'Data to update can not be empty!',
+    });
+  }
+  const userId = new ObjectId(req.params.id);
+  const person = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+  const response = await mongodb.getDb().db().collection('people').replaceOne({ _id: userId }, person);
+  console.log(response);
+  if (response.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while updating the person in contacts.');
+  }
+};
+
+module.exports = { getSingleData, getData, createNewContact, updatePerson };
--- routes/contacts.js
+++ routes/contacts.js
@@ -10,6 +10,13 @@ router.get('/', contactsController.getData);
 
 router.get('/:id', contactsController.getSingleData);
 
+router.post('/', contactsController.createNewContact);
+
+router.put('/:id', contactsController.updatePerson);
+
+router.delete('/:id', contactsController.deletePerson);
+
+
 
 // localhost:8080
 module.exports = router;
