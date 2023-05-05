26c4faed615cd0f922fa1656b28d045e64fcf560
HonorThe1nsane <troymd2009@hotmail.com>
2023-05-05 12:34:39 -0600

    update

--- routes/contacts.js
+++ routes/contacts.js
@@ -8,6 +8,13 @@ const contactsController = require('../controllers/contacts');
 
 router.get('/', contactsController.getData);
 
+router.post('/', contactsController.createNewContact);
+
+router.put('/:id', contactsController.updatePerson);
+
+router.delete('/:id', contactsController.deletePerson);
+
+
 // localhost:8080
 module.exports = router;
 
