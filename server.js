2132b62ffa47a073ed23d3cf349554a86c4afb32
HonorThe1nsane <troymd2009@hotmail.com>
2023-05-10 13:29:00 -0600

    added access for REACT

--- server.js
+++ server.js
@@ -21,6 +21,9 @@ app
 app.use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Credentials', true);
     next();
   })
   .use('/', require('./routes'));
