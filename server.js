610d5096805aeb0a201e291db5015f428fe2f364
HonorThe1nsane <troymd2009@hotmail.com>
2023-05-10 13:33:54 -0600

    test

--- server.js
+++ server.js
@@ -23,11 +23,10 @@ app.use(bodyParser.json())
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Credentials', true);
+    // res.setHeader('Access-Control-Allow-Credentials', true);
     next();
   })
   .use('/', require('./routes'));
-
 
 
 mongodb.initDb((err, mongodb) => {
